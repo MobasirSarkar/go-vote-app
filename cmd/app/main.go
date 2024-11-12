413cfbf7ac62a6f5e092b1485aec1a1cb3ecc3bf
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-11-12 20:25:09 +0530

    test

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"log"
-
+	"fmt"
 	"github.com/MobasirSarkar/go-vote-app/internal/server"
 )
 
 func main() {
 	server := server.NewServer()
+	fmt.Println("Hello")
 
 	err := server.ListenAndServe()
 	if err != nil {
